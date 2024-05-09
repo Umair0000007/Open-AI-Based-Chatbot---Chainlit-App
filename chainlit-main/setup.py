38f6d321e916ad33eaa6ea44d76ea37d0855f784
Umair Mansoor <110084323+Umair0000007@users.noreply.github.com>
2024-05-09 14:02:17 +0500

    Update setup.py

--- chainlit-main/setup.py
+++ chainlit-main/setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name = 'zomato-bot',
     version= '0.0.1',
-    author= 'sunny savita',
-    author_email= 'sunny.savita@gmail.com',
+    author= 'umair mansoor',
+    author_email= 'umairmansoor116@gmail.com',
     packages= find_packages(),
     install_requires = ["chainlit","notebook","ipywidgets","tqdm","python-dotenv","langchain-google-genai"]
 
